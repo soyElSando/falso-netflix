fb9a5b1c2ef09578124363753f23b458d1ecdd58
Romina Pizzuti <pizzutiromina@gmail.com>
2022-10-15 22:36:18 +0200

    Prueba de modificacion

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Modificacion Romi
         </a>
       </header>
     </div>
