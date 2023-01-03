7d0e0da13b337bdc73d6074815996417efee330f
soyElSando <german.tito.sandoval@gmail.com>
2023-01-03 09:07:03 -0300

    auth_1.3

--- src/features/Login/login.services.js
+++ src/features/Login/login.services.js
@@ -1,11 +1,13 @@
 import { AUTHAPI } from "../../commons";
 
 export const loginService = async (email, password) => {
-  const res = await AUTHAPI.api.post(AUTHAPI.paths.login, { email, password });
+  //const res = await AUTHAPI.api.post(AUTHAPI.paths.login, { email, password });
+  setTimeout(()=>{res.data=email},500)
   return res.data;
 };
 
 export const logoutService = async () => {
-    const res = await AUTHAPI.api.post(AUTHAPI.paths.logout);
+    //const res = await AUTHAPI.api.post(AUTHAPI.paths.logout);
+    setTimeout(()=>{res.data="ok"},500)
     return res.data;
   };
